88c8a71625feffdf253d8b1cb7fcc6209a7857e5
Amir <Amirgee007@yahoo.com>
2023-12-26 23:52:19 +0500

    large image also should be editable here now

--- resources/views/products/inventory/index.blade.php
+++ resources/views/products/inventory/index.blade.php
@@ -316,6 +316,25 @@
             </div>
         </div>
     </div>
+
+    <div class="modal" id="largeImgModal" tabindex="-1" role="dialog">
+        <div class="modal-dialog modal-lg" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title">Large Image</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    <img src="" id="imagePreviewId" style="width: 650px; height: 400px;" >
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                </div>
+            </div>
+        </div>
+    </div>
 @stop
 
 @section('scripts')
