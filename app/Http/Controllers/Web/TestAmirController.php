7073878b977755cfc752112d99dec2b3764e4b63
Amir <Amirgee007@yahoo.com>
2024-04-24 02:40:44 +0500

    updated emaila ddress too

--- app/Http/Controllers/Web/TestAmirController.php
+++ app/Http/Controllers/Web/TestAmirController.php
@@ -26,10 +26,18 @@ class TestAmirController extends Controller
         $user = User::first();
         $salesRepEmail = getSalesRepsNameEmail($user->sales_rep);
 
-        $content = 'User chnaged his shipping address please check asap i.e user is '.$user->first_name;
-        \Mail::to('christinah@virginfarms.com')
-            ->bcc('amir@infcompany.com')
-            ->send(new VirginFarmGlobalMail('Hi, plz check some user updated shipping address', $content));
+        $salesRepEmail = getSalesRepsNameEmail($user->sales_rep);
+
+        $content = 'User Changed his shipping address under profile page please check asap i.e user is '.$user->first_name;
+
+        \Mail::raw($content, function ($message) use($salesRepEmail) {
+            $message->to('christinah@virginfarms.com')
+                ->bcc(['amir@infcompany.com' , $salesRepEmail])
+                ->subject('Hi, plz check some user updated shipping address');});
+
+//        \Mail::to('christinah@virginfarms.com')
+//            ->bcc('amir@infcompany.com')
+//            ->send(new VirginFarmGlobalMail('Hi, plz check some user updated shipping address', $content));
 
         dd('dd');
 
--- app/Repositories/User/EloquentUser.php
+++ app/Repositories/User/EloquentUser.php
@@ -142,10 +142,12 @@ class EloquentUser implements UserRepository
 
             $salesRepEmail = getSalesRepsNameEmail($user->sales_rep);
 
-            $content = 'User chnaged his shipping address please check asap i.e user is '.$user->first_name;
-            \Mail::to('christinah@virginfarms.com')
-                ->bcc('amir@infcompany.com')
-                ->send(new VirginFarmGlobalMail('Hi, plz check some user updated shipping address', $content));
+            $content = 'User Changed his shipping address under profile page please check asap i.e user is '.$user->first_name;
+
+            \Mail::raw($content, function ($message) use($salesRepEmail) {
+                $message->to('christinah@virginfarms.com')
+                    ->bcc(['amir@infcompany.com' , $salesRepEmail])
+                    ->subject('Hi, plz check some user updated shipping address');});
         }
 
         return $user;
