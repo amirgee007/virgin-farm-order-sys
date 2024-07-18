ae03c18356edc9fbbe8c7dcf7e9b878c44ef410e
Amir <Amirgee007@yahoo.com>
2024-07-18 20:48:36 +0500

    updated and added a feature to see users only his, him orders

--- app/Http/Controllers/Web/OrdersController.php
+++ app/Http/Controllers/Web/OrdersController.php
@@ -34,20 +34,20 @@ class OrdersController extends Controller
         $users = [$user_id => auth()->user()->first_name];
 
         #client showing and else is in ADMIN.
-//        if($user_id)
-//            $query->where('user_id' , $user_id);
-//        else{
-//            $users = User::where('status' , UserStatus::ACTIVE)
-//                ->orderby('first_name')
-//                ->pluck('first_name', 'id')
-//                ->toArray();
-//
-//            $users = [0 => 'Show All']+$users;
-//        }
+        if($user_id)
+            $query->where('user_id' , $user_id);
+        else{
+            $users = User::where('status' , UserStatus::ACTIVE)
+                ->orderby('first_name')
+                ->pluck('first_name', 'id')
+                ->toArray();
 
-//        if($user){
-//            $query->where('user_id' , $user);
-//        }
+            $users = [0 => 'Show All']+$users;
+        }
+
+        if($user){
+            $query->where('user_id' , $user);
+        }
 
         if ($sales_rep) {
             $query->where('sales_rep', $sales_rep);
