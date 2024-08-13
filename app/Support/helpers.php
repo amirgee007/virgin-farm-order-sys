b59f1ad169573020239325906e75ba5e6b0adfe5
Amir <Amirgee007@yahoo.com>
2024-08-14 01:07:03 +0500

    Attempt to read property "carrier_id" on null {"view":,"exception":"[object] (Spatie\\LaravelIgnition\\Exceptions\\ViewException(code: 0): Attempt to read property \"carrier_id\" on null at /var/www/vhosts/virginfarms.net/httpdocs/html/virginfarms.net/app/Support/helpers.php:190)

--- app/Support/helpers.php
+++ app/Support/helpers.php
@@ -187,7 +187,7 @@ function checkIfSkipCubeRangeCondition()
     $response = false;
 
     #Yes, cube range requirement applies only to fedex carrier method.
-    if($user->carrier_id != 23 || $user->edit_order_id) #because 1 is add-on new order
+    if($user && ($user->carrier_id != 23 || $user->edit_order_id)) #because 1 is add-on new order
         $response = true;
 
     return  $response;
