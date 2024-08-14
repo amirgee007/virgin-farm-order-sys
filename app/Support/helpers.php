15275da77b29186021afa22253e02e7992ba0e1e
Amir <Amirgee007@yahoo.com>
2024-08-15 01:59:43 +0500

    just indent

--- app/Support/helpers.php
+++ app/Support/helpers.php
@@ -33,102 +33,101 @@ function cartTimeLeftSec()
     return $remainingSeconds > 0 ? $remainingSeconds : 0;
 
 }
-function myPriceColumn(){
+
+function myPriceColumn()
+{
     #OPTIMIZATION required plz keep in mind to reduce conditions
     $user = itsMeUser();
     $prices = getPrices();
 
-    $column = $user->price_list ? $prices[$user->price_list]  : 'price_fedex';
+    $column = $user->price_list ? $prices[$user->price_list] : 'price_fedex';
 
     #if price is FEDEX and carrier  other than fedex then price fob
-    if($user->price_list == 1 && $user->carrier_id != 23)
+    if ($user->price_list == 1 && $user->carrier_id != 23)
         $column = 'price_fob';
 
     #if price is FOB and carrier FedEx then use fedex price
-    elseif($user->price_list == 2 && $user->carrier_id == 23)
+    elseif ($user->price_list == 2 && $user->carrier_id == 23)
         $column = 'price_fedex';
 
     #if price is Hawai and carrier other than FedEx then use fob
-    elseif($user->price_list == 3 && $user->carrier_id != 23)
+    elseif ($user->price_list == 3 && $user->carrier_id != 23)
         $column = 'price_fob';
 
-       return $column;
+    return $column;
 }
 
-function isDeliveryChargesApply(){
+function isDeliveryChargesApply()
+{
     $user = itsMeUser();
     $note = null;
 
     #Carrier not PU(32), Not Federal Express(23) New LOGIC
-    if(!in_array($user->carrier_id , [23,32]))
+    if (!in_array($user->carrier_id, [23, 32]))
         $note = 'Delivery charges may apply.';
 
     return $note;
 }
 
-function getCubeSizeTax($size){
+function getCubeSizeTax($size)
+{
 
     #PU(32), Federal Express(23) , DLV(17)
     $user = itsMeUser();
 
-    $salesRepExtra = in_array($user->sales_rep , ['Robert', 'Mario', 'Joe']);
+    $salesRepExtra = in_array($user->sales_rep, ['Robert', 'Mario', 'Joe']);
 
     $tax = $boxChargesApply = $serviceTransportFees24 = $additional = $extraTax = 0;
 
     #If carrier is not Federal Express then apply 24%
-    if($user->carrier_id != 23)
+    if ($user->carrier_id != 23)
         $serviceTransportFees24 = true;
 
     #Carrier is fedex then apply BOX CHARGES ONLY one case
-    if($user->carrier_id == 23)
+    if ($user->carrier_id == 23)
         $boxChargesApply = true;
 
-    if($boxChargesApply){
-        if($size >= 12 && $size <= 16){
+    if ($boxChargesApply) {
+        if ($size >= 12 && $size <= 16) {
             $tax = 32;
             $extraTax = 1;
-        }
-        elseif($size >= 18 && $size <= 21) {
+        } elseif ($size >= 18 && $size <= 21) {
             $tax = 35;
             $extraTax = 1;
-        }
-        elseif($size >= 22 && $size <= 25) {
+        } elseif ($size >= 22 && $size <= 25) {
             $tax = 33;
             $extraTax = 1;
-        }
-        elseif($size >= 27 && $size <= 30) {
+        } elseif ($size >= 27 && $size <= 30) {
             $tax = 34.5;
             $extraTax = 1;
-        }
-        elseif($size >= 40 && $size <= 45) {
+        } elseif ($size >= 40 && $size <= 45) {
             $tax = 36;
             $extraTax = 2;
-        }
-        elseif($size > 45) {
+        } elseif ($size > 45) {
             $tax = 36;
             $extraTax = 2;
         }
 
-        if($salesRepExtra)
+        if ($salesRepExtra)
             $tax = $tax + $extraTax;
 
-        if($size/45 > 1){
-            $countMore45 = ((int)ceil($size/45) - 1);
+        if ($size / 45 > 1) {
+            $countMore45 = ((int)ceil($size / 45) - 1);
             $additional = 33 * $countMore45;
         }
     }
 
-    if($serviceTransportFees24)
+    if ($serviceTransportFees24)
         $tax = $size * 0.24; #fixed 0.24 Example: 45 cubes * 0.24 = $10.80
 
-    $total =  round2Digit($additional + $tax);
+    $total = round2Digit($additional + $tax);
     $extra = 0;
 
-    try{
+    try {
         #checked if week is during the PRICES then up it too.
-        $found = Setting::where('key' , 'extra-fees-date')->where('value' , '>' , 0)->first();
-        if($found){
-            $dates = json_decode($found->label , true);
+        $found = Setting::where('key', 'extra-fees-date')->where('value', '>', 0)->first();
+        if ($found) {
+            $dates = json_decode($found->label, true);
 
             $start = Carbon::parse($dates['date_in']);
             $end = Carbon::parse($dates['date_out']);
@@ -139,14 +138,15 @@ function getCubeSizeTax($size){
                 $extra = round2Digit(($found->value / 100) * $total);
             }
         }
-    }catch (\Exception $ex){
+    } catch (\Exception $ex) {
         Log::error($ex->getMessage() . ' error calcualting extra percentage data/values etc during transportation. ');
     }
 
-    return $total+$extra;
+    return $total + $extra;
 }
 
-function divideIntoGroupMax($number, $groupSize = 45) {
+function divideIntoGroupMax($number, $groupSize = 45)
+{
     $result = [];
 
     while ($number > 0) {
@@ -168,31 +168,31 @@ function getCubeRanges($total)
 
     #if customer is Just for FOB when PU is carrier then no need to do the CUBE sizes
     $maxValue = 45;
-    $max = Box::orderBy('max_value' , 'desc')->first();
-    if($max && $max->max_value)
+    $max = Box::orderBy('max_value', 'desc')->first();
+    if ($max && $max->max_value)
         $maxValue = $max->max_value;
 
     #logic is check if less than 45 then check the limit between all current boxes otherwise devide into 45, 45 and then check all.
-    $values = $total > $maxValue ? divideIntoGroupMax($total , $maxValue) : [$total];
+    $values = $total > $maxValue ? divideIntoGroupMax($total, $maxValue) : [$total];
     $matched = [];
 
-    foreach ($values as $value){
+    foreach ($values as $value) {
         #Restrict Hawaii and Alaska customers only, cannot purchase the medium box. Must be above 22 cubes (medium large boxes and up).
         if ($stateNotAllow22)
             $found = Box::where('min_value', '>', 22)->where('max_value', '>=', $value)->first();
         else
             $found = Box::where('min_value', '<=', $value)->where('max_value', '>=', $value)->first();
 
-        if($found)
+        if ($found)
             $matched[] = $found->description;
     }
 
     #if($address_id) $shipAddress = ShippingAddress::find($address_id);
 
-    if(checkIfSkipCubeRangeCondition())
-        return [$matched , true];
+    if (checkIfSkipCubeRangeCondition())
+        return [$matched, true];
 
-    return (count($values) == count($matched)) ? [$matched , true] : [$matched , false];
+    return (count($values) == count($matched)) ? [$matched, true] : [$matched, false];
 }
 
 function checkIfSkipCubeRangeCondition()
@@ -201,13 +201,14 @@ function checkIfSkipCubeRangeCondition()
     $response = false;
 
     #Yes, cube range requirement applies only to fedex carrier method.
-    if($user && ($user->carrier_id != 23 || $user->edit_order_id)) #because 1 is add-on new order
+    if ($user && ($user->carrier_id != 23 || $user->edit_order_id)) #because 1 is add-on new order
         $response = true;
 
-    return  $response;
+    return $response;
 }
 
-function getPrices(){
+function getPrices()
+{
     #if any change plz check this too 1,2,3 getCubeSizeTax
     return [
         0 => 'Select Price',
@@ -217,10 +218,13 @@ function getPrices(){
     ];
 }
 
-function getCarriers(){
+function getCarriers()
+{
     return Carrier::pluck('carrier_name', 'id')->sortBy('c_code')->toArray();
 }
-function getStates(){
+
+function getStates()
+{
 
     $states = UsState::orderby('state_name')
         ->pluck('state_name', 'id')
@@ -228,29 +232,39 @@ function getStates(){
 
     $states = [null => 'Select State'] + $states;
 
-    return  $states;
+    return $states;
 }
 
-function round2Digit($number){
+function round2Digit($number)
+{
     $roundedValue = round(floatval($number), 2);
     return number_format($roundedValue, 2, '.', '');
 }
-function diff4Human($date ){
+
+function diff4Human($date)
+{
     return is_null($date) ? 'n/a' : Carbon::parse($date)->diffForHumans();
 }
-function getTerms(){
-    return  [
+
+function getTerms()
+{
+    return [
         'N1', 'CC', 'Check by Phone'
     ];
 }
-function myRoleName(){
-    return  auth()->user() ? auth()->user()->role->name : '';
+
+function myRoleName()
+{
+    return auth()->user() ? auth()->user()->role->name : '';
 }
-function itsMeUser(){
+
+function itsMeUser()
+{
     return \Vanguard\User::find(auth()->id());
 }
 
-function getSalesReps(){
+function getSalesReps()
+{
     #if any change plz check this too 1,2,3 getCubeSizeTax
     return [
         '0' => 'Select Sales Rep',
@@ -277,12 +291,13 @@ function getSalesRepsNameEmail($name)
     return $emails[$name] ?? 'amirseersol@gmail.com';
 }
 
-function checkAvailableQty($product_id){
+function checkAvailableQty($product_id)
+{
 
     $user = itsMeUser();
     $date_shipped = $user->last_ship_date;
 
-    return ProductQuantity::where('product_quantities.product_id' , $product_id)
+    return ProductQuantity::where('product_quantities.product_id', $product_id)
         ->leftjoin('carts', 'carts.product_id', '=', 'product_quantities.id')
         ->whereRaw('"' . $date_shipped . '" between `date_in` and `date_out`')
         ->selectRaw('product_quantities.quantity-COALESCE(carts.quantity, 0) as quantity')
@@ -303,15 +318,18 @@ function cleanArray($array)
     return $result;
 }
 
-function dateFormatMy($date){
+function dateFormatMy($date)
+{
     return Carbon::parse($date)->format('m/d/Y');
 }
 
-function dateFormatRecent($date){
+function dateFormatRecent($date)
+{
     return Carbon::parse($date)->toFormattedDayDateString();
 }
 
-function dateRangeConverter($dateInOut){
+function dateRangeConverter($dateInOut)
+{
 
     $date_range = explode("-", $dateInOut);
 
@@ -329,14 +347,14 @@ function dateRangeConverter($dateInOut){
 function getAddOnDetail($order)
 {
     $text = '';
-    try{
-        if($order->full_add_on == 1)
+    try {
+        if ($order->full_add_on == 1)
             $text = 'Add-On General';
-        if($order->full_add_on == 2)
-            $text = 'Add-On #W-'.$order->id;
+        if ($order->full_add_on == 2)
+            $text = 'Add-On #W-' . $order->id;
 
-    }catch (\Exception $ex){
-        Log::error('something went wrong during get add on detail '.$ex->getMessage());
+    } catch (\Exception $ex) {
+        Log::error('something went wrong during get add on detail ' . $ex->getMessage());
     }
 
     return $text;
@@ -344,17 +362,19 @@ function getAddOnDetail($order)
 
 #if user id is zero then it means its for ADMIN
 
-function addOwnNotification($message ,$order_id = 0, $user_id = 0 ){
+function addOwnNotification($message, $order_id = 0, $user_id = 0)
+{
     $data = [
         'message' => $message,
         'order_id' => $order_id,
         'user_id' => $user_id
     ];
 
-    ClientNotification::updateOrcreate($data , $data);
+    ClientNotification::updateOrcreate($data, $data);
 }
 
-function isReadFAQ(){
+function isReadFAQ()
+{
     return auth()->user()->announcements_last_read_at;
 
 //    auth()->user()->forceFill([
