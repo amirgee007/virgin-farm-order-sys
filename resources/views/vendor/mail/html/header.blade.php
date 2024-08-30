dd2fab1535b754239a8ac079395ce9b2d4ea42c1
Amir <Amirgee007@yahoo.com>
2024-08-30 05:31:20 +0500

    D:\wamp64\www\Laravel_projects\virgin-farm-order-sys\public\assets\img\

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -1,5 +1,5 @@
 <tr>
     <td class="header">
-        <img class="rounded-circle img-thumbnail" src="{{ asset('assets/img/inf_logo.jpg') }}" alt="INF" >
+        <img class="rounded-circle img-thumbnail" src="{{ asset('assets/img/vanguard-logo.png') }}" alt="INF" >
     </td>
 </tr>
