a42ca98747ace9093a32058a1be99e752d804b6c
Amir <Amirgee007@yahoo.com>
2025-01-09 01:11:36 +0500

    updated studff

--- resources/views/products/report.blade.php
+++ resources/views/products/report.blade.php
@@ -10,20 +10,19 @@
     <address style="text-align: right; margin: 0;">
         <b>Virgin Farms Direct</b><br>
         1.888.548 (7673)<br>
-        www.virginfarms.com<br>
-        www.virginfarms.net<br>
-        sales@virginfarms.com<br>
-        weborders@virginfarms.com<br>
+        <a href="https://www.virginfarms.com" target="_blank">www.virginfarms.com</a><br>
+        <a href="https://www.virginfarms.net" target="_blank">www.virginfarms.net</a><br>
+        <a href="mailto:sales@virginfarms.com">sales@virginfarms.com</a><br>
+        <a href="mailto:weborders@virginfarms.com">weborders@virginfarms.com</a><br>
     </address>
 </div>
 
 <h4>Availability Week of {{@dateFormatMy($dateIn)}}</h4>
 <table border="0.5px" width="100%">
     <thead>
-
     <tr>
         @foreach ($columns as $column)
-            <th style="text-align: center;">{{ @$columnCustomNames[$column] }}</th>
+            <th style="text-align: {{ $column === 'product_text' ? 'left' : 'center' }};">{{ @$columnCustomNames[$column] }}</th>
         @endforeach
     </tr>
     </thead>
@@ -31,9 +30,9 @@
     @foreach ($data as $row)
         <tr>
             @foreach ($columns as $column)
-                <td style="text-align: center;">
+                <td style="text-align: {{ $column === 'product_text' ? 'left' : 'center' }};">
                     @if (str_contains($column, 'price'))
-                        {{ round2Digit($row[$column]) }}
+                        ${{ round2Digit($row[$column]) }}
                     @else
                         {{ $row[$column] }}
                     @endif
@@ -43,5 +42,6 @@
     @endforeach
     </tbody>
 </table>
+
 </body>
 </html>
