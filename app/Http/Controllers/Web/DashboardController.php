41d3c52b9a0176c1439889b5062a24a82f926de2
Amir <Amirgee007@yahoo.com>
2025-01-28 00:23:33 +0500

    hide orders from main page.

--- app/Http/Controllers/Web/DashboardController.php
+++ app/Http/Controllers/Web/DashboardController.php
@@ -26,7 +26,12 @@ class DashboardController extends Controller
             session()->flash('success', __('E-Mail verified successfully.'));
         }
 
-        $orders = Order::where('is_active' , 1)->orderBy('date_shipped')->limit(10)->get();
+        $user_id = myRoleName() == 'Admin' ? null : auth()->id();
+        $query = $orders = Order::where('is_active', 1)->orderBy('date_shipped');
+        if ($user_id)
+            $query->where('user_id', $user_id);
+
+        $orders = $query->limit(10)->get();
         #$future_inventory = ProductQuantity::query()->groupBy(['date_in', 'date_out'])->get();
 
         $futureInventory = \DB::SELECT('
@@ -47,20 +52,22 @@ class DashboardController extends Controller
         #$lowInventory = ProductQuantity::where('quantity' , 0)->where('date_out' , '>' , now()->toDateString())->limit(100)->get();
         $lowInventory = [];
 
-        return view('dashboard.index' , compact('orders' , 'futureInventory' , 'lowInventory'));
+        return view('dashboard.index', compact('orders', 'futureInventory', 'lowInventory'));
     }
 
-    public function updateSupplier(Request $request){
+    public function updateSupplier(Request $request)
+    {
         // Store the selected supplier in the user preferences
         $user = auth()->user();
         $user->supplier_id = $request->input('supplier');
         $user->save();
 
         #, You will be redirected to inventory page.
-        return response()->json(['message' => 'Supplier updated successfully.' , 'href' => route('inventory.index')]);
+        return response()->json(['message' => 'Supplier updated successfully.', 'href' => route('inventory.index')]);
     }
 
-    public function updateFaqRead(Request $request){
+    public function updateFaqRead(Request $request)
+    {
 
         // Store the selected supplier in the user preferences
         auth()->user()->forceFill([
@@ -71,9 +78,10 @@ class DashboardController extends Controller
         return response()->json(['message' => 'Settings updated successfully.']);
     }
 
-    public function checkAdminUploadingFiles(){
+    public function checkAdminUploadingFiles()
+    {
         // Replace 'your_table' and 'status_column' with your actual table and column
-        $status = Setting::where('key' , 'admin-uploading')->where('value' , 1)->first();
+        $status = Setting::where('key', 'admin-uploading')->where('value', 1)->first();
 
         // Assume status 1 means block interactions, 0 means allow interactions
         if ($status && myRoleName() != 'Admin') {
