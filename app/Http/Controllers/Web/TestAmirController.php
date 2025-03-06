62d378569a3633bcb4fb23d398a10e57f5f2b952
Amir <Amirgee007@yahoo.com>
2025-03-07 01:04:22 +0500

    keep email simple

--- app/Http/Controllers/Web/TestAmirController.php
+++ app/Http/Controllers/Web/TestAmirController.php
@@ -25,17 +25,6 @@ class TestAmirController extends Controller
     public function index3()
     {
 
-        $user = User::find(1);
-        $verificationUrl = \URL::temporarySignedRoute(
-            'verification.verify',
-            now()->addMinutes(60),
-            ['id' => $user->id, 'hash' => sha1($user->email)]
-        );
-
-        dd($verificationUrl);
-
-
-
 
         $user = User::query()->inRandomOrder()->first();
 
--- resources/views/mail/info-email.blade.php
+++ resources/views/mail/info-email.blade.php
@@ -3,13 +3,13 @@
     off on your first purchase!</p>
 <p><b>Here are some instructions.</b></p>
 <ul>
-    <li>1. You can use any email address to register.</li>
-    <li>2. After registering, check your inbox to verify your email.</li>
-    <li>3. Until you verify your email, your status will remain unverified.</li>
-    <li>4. Once your email is verified, you can attempt to log in.</li>
-    <li>5. Your account needs admin approval before full access.</li>
-    <li>6. You can browse products, but purchasing is only available after approval.</li>
+    <li>1. After registering, check your inbox to verify your email.</li>
+    <li>2. Until you verify your email, your status will remain unverified.</li>
+    <li>3. Once your email is verified, you can attempt to log in.</li>
+    <li>4. Your account needs admin approval before full access.</li>
+    <li>5. You can browse products, but purchasing is only available after approval.</li>
     <li>6. During this time, you can also access the help page.</li>
     <li>7. After admin approval, you'll receive an email confirmation.</li>
     <li>8. Only then can you log in, make purchases, and use other features of our webshop.</li>
 </ul>
+
