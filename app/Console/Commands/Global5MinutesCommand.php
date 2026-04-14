5a3dd202e6255f2213a6d84d50de1bab582efbbb
hfzRamzi <hfzramzi@virginfarms.net>
2026-04-14 18:38:00 +0500

    users

--- app/Console/Commands/Global5MinutesCommand.php
+++ app/Console/Commands/Global5MinutesCommand.php
@@ -8,6 +8,7 @@ use Illuminate\Support\Facades\Log;
 use Vanguard\Models\Cart;
 use Vanguard\Models\ProductQuantity;
 use Illuminate\Support\Facades\DB;
+use Vanguard\User;
 
 class Global5MinutesCommand extends Command
 {
@@ -92,6 +93,7 @@ class Global5MinutesCommand extends Command
         }
 
         $activeSessionCutoff = now()->subMinutes(config('session.lifetime'))->timestamp;
+
 
         User::query()
             ->whereNotNull('last_ship_date')
